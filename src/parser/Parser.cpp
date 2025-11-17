39423337e96a95edf81f0ae76dff94db563371e1
Roman <rk.salimgareev@mail.ru>
2025-11-17 22:25:54 +0300

    Fixed style functions and variables

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -2,66 +2,66 @@
 #include <stdexcept>
 #include <type_traits>
 
-enum class TypeCommand {ADD, LIST, CLEAR, DONE, REMOVE, EDIT, CONFIG, HELP};
-
-static TypeCommand formatInEnum(const std::string type)
-{
-    if (type == "add") 
-        return TypeCommand::ADD;
-    else if (type == "list")
-        return TypeCommand::LIST;
-    else if (type == "clear")
-        return TypeCommand::CLEAR;
-    else if (type == "done")
-        return TypeCommand::DONE;
-    else if (type == "remove")
-        return TypeCommand::REMOVE;
-    else if (type == "edit")
-        return TypeCommand::EDIT;
-    else if (type == "config")
-        return TypeCommand::CONFIG;
-    else if (type == "help")
-        return TypeCommand::HELP;
-    else 
-    {
-        const std::string message = "Error: unknow command";
-        throw std::runtime_error(message);
-    }
-}
-
-static void isValidCommandWords(const TypeCommand& checkType, int count)
-{
-    bool isTypeChecked = (checkType == TypeCommand::ADD) || 
-                   (checkType == TypeCommand::DONE) || 
-                   (checkType == TypeCommand::REMOVE) || 
-                   (checkType == TypeCommand::EDIT) || 
-                   (checkType == TypeCommand::CONFIG);
-
-    if ((count < 3 || count > 3) && isTypeChecked)
-    {
-        const std::string message = "Error: invalid number of command arguments";
-        throw std::runtime_error(message);
-    }
-}
-
-static int convertWordInNumber(const std::string& str)
-{
-    size_t pos = 0;
-    int number = std::stoi(str, &pos);
-    if (pos != str.size())
-        throw std::runtime_error("Error: Extract chars");
-    return number;
-}
-
 namespace parse
 {
+    enum class TypeCommand {ADD, LIST, CLEAR, DONE, REMOVE, EDIT, CONFIG, HELP};
+
+    static TypeCommand FormatInEnum(const std::string type)
+    {
+        if (type == "add") 
+            return TypeCommand::ADD;
+        else if (type == "list")
+            return TypeCommand::LIST;
+        else if (type == "clear")
+            return TypeCommand::CLEAR;
+        else if (type == "done")
+            return TypeCommand::DONE;
+        else if (type == "remove")
+            return TypeCommand::REMOVE;
+        else if (type == "edit")
+            return TypeCommand::EDIT;
+        else if (type == "config")
+            return TypeCommand::CONFIG;
+        else if (type == "help")
+            return TypeCommand::HELP;
+        else 
+        {
+            const std::string message = "Error: unknow command";
+            throw std::runtime_error(message);
+        }
+    }
+
+    static void ValidCommandWords(const TypeCommand& check_type, int count)
+    {
+        bool is_type_checked = (check_type == TypeCommand::ADD) || 
+                    (check_type == TypeCommand::DONE) || 
+                    (check_type == TypeCommand::REMOVE) || 
+                    (check_type == TypeCommand::EDIT) || 
+                    (check_type == TypeCommand::CONFIG);
+
+        if ((count < 3 || count > 3) && is_type_checked)
+        {
+            const std::string message = "Error: invalid number of command arguments";
+            throw std::runtime_error(message);
+        }
+    }
+
+    static int ConvertWordInNumber(const std::string& str)
+    {
+        size_t pos = 0;
+        int number = std::stoi(str, &pos);
+        if (pos != str.size())
+            throw std::runtime_error("Error: Extract chars");
+        return number;
+    }
+
     void Parser::Parse(const int& argc, const char** argv)
     {
         if (argc < 2)
             throw std::runtime_error("Error: no command entered");
         
-        TypeCommand type = formatInEnum(std::string(argv[1]));
-        isValidCommandWords(type, argc);
+        TypeCommand type = FormatInEnum(std::string(argv[1]));
+        ValidCommandWords(type, argc);
         switch (type)
         {
             case TypeCommand::ADD:
@@ -84,13 +84,13 @@ namespace parse
             case TypeCommand::DONE:
             {    
                 command.type_ = "done";
-                command.id_ = convertWordInNumber(argv[2]);
+                command.id_ = ConvertWordInNumber(argv[2]);
                 break;
             }
             case TypeCommand::REMOVE:
             {    
                 command.type_ = "remove";
-                command.id_ = convertWordInNumber(argv[2]);
+                command.id_ = ConvertWordInNumber(argv[2]);
                 break;
             }
             case TypeCommand::EDIT:
