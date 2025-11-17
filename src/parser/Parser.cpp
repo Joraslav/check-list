ec55fe48557be2c3862e0acb8e4b5f30d74f998f
Roman <rk.salimgareev@mail.ru>
2025-11-17 22:02:06 +0300

    Changed class Parser and added new functions

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1,6 +1,115 @@
 #include "Parser.hpp"
+#include <stdexcept>
+#include <type_traits>
+
+enum class TypeCommand {ADD, LIST, CLEAR, DONE, REMOVE, EDIT, CONFIG, HELP};
+
+static TypeCommand formatInEnum(const std::string type)
+{
+    if (type == "add") 
+        return TypeCommand::ADD;
+    else if (type == "list")
+        return TypeCommand::LIST;
+    else if (type == "clear")
+        return TypeCommand::CLEAR;
+    else if (type == "done")
+        return TypeCommand::DONE;
+    else if (type == "remove")
+        return TypeCommand::REMOVE;
+    else if (type == "edit")
+        return TypeCommand::EDIT;
+    else if (type == "config")
+        return TypeCommand::CONFIG;
+    else if (type == "help")
+        return TypeCommand::HELP;
+    else 
+    {
+        const std::string message = "Error: unknow command";
+        throw std::runtime_error(message);
+    }
+}
+
+static void isValidCommandWords(const TypeCommand& checkType, int count)
+{
+    bool isTypeChecked = (checkType == TypeCommand::ADD) || 
+                   (checkType == TypeCommand::DONE) || 
+                   (checkType == TypeCommand::REMOVE) || 
+                   (checkType == TypeCommand::EDIT) || 
+                   (checkType == TypeCommand::CONFIG);
+
+    if ((count < 3 || count > 3) && isTypeChecked)
+    {
+        const std::string message = "Error: invalid number of command arguments";
+        throw std::runtime_error(message);
+    }
+}
+
+static int convertWordInNumber(const std::string& str)
+{
+    size_t pos = 0;
+    int number = std::stoi(str, &pos);
+    if (pos != str.size())
+        throw std::runtime_error("Error: Extract chars");
+    return number;
+}
 
 namespace parse
 {
-    // 
+    void Parser::Parse(const int& argc, const char** argv)
+    {
+        if (argc < 2)
+            throw std::runtime_error("Error: no command entered");
+        
+        TypeCommand type = formatInEnum(std::string(argv[1]));
+        isValidCommandWords(type, argc);
+        switch (type)
+        {
+            case TypeCommand::ADD:
+            {    
+                command.type_ = "add";
+                for (int i = 2; i < argc; ++i)
+                    command.task_text_ += " " + std::string(argv[i]);
+                break;
+            }
+            case TypeCommand::LIST:
+            {    
+
+                break;
+            }
+            case TypeCommand::CLEAR:
+            {    
+                command.type_ = "clear";
+                break;
+            }
+            case TypeCommand::DONE:
+            {    
+                command.type_ = "done";
+                command.id_ = convertWordInNumber(argv[2]);
+                break;
+            }
+            case TypeCommand::REMOVE:
+            {    
+                command.type_ = "remove";
+                command.id_ = convertWordInNumber(argv[2]);
+                break;
+            }
+            case TypeCommand::EDIT:
+            {    
+
+                break;
+            }
+            case TypeCommand::CONFIG:
+            {   
+
+                break;
+            }
+            case TypeCommand::HELP:
+            {   
+                command.type_ = "help";
+                break;
+            }
+            default:
+                break;
+        }   
+    }
 } // namespace parse
--- src/parser/Parser.hpp
+++ src/parser/Parser.hpp
@@ -6,16 +6,29 @@
 
 namespace parse {
 
-class Parser {
- public:
-    Parser(const std::string& command) : command_(command) {};
+class Parser 
+{
+   public:
+      Parser() = default;
 
-    const std::string& GetAllTaskText(int argc, std::string_view argv) const;
+      const std::string GetType() const { return command.type_; }
+      
+      const std::string GetTaskText() const { return command.task_text_; }
 
- private:
-    std::string command_;
-    std::string task_text_;
-    std::string filter_;
+      const int GetId() const { return command.id_;}
+
+      void Parse(const int& argc, const char** argv);
+      
+      //const std::string& GetAllTaskText(int argc, std::string_view argv) const;
+   private:
+      struct Command
+      {
+         //enum Type {ADD, DELETE, LIST, ANOTHER} type_ = ANOTHER;
+         std::string type_ = "";
+         std::string task_text_ = "";
+         int id_ = -1;
+      };
+      Command command;
 };
 
 }  // namespace parse
