cd07666ddd488b7813388157da681549e425db2a
Roman <rk.salimgareev@mail.ru>
2025-11-20 20:26:05 +0300

    add new convert and check function

--- src/parser/Parser.hpp
+++ src/parser/Parser.hpp
@@ -4,31 +4,28 @@
 #include <string_view>
 #include <vector>
 
-namespace parse {
-
-class Parser 
+namespace parser 
 {
-   public:
-      Parser() = default;
+   class Parser 
+   {
+      public:
+         Parser() = default;
 
-      const std::string GetType() const { return command.type_; }
-      
-      const std::string GetTaskText() const { return command.task_text_; }
+         const std::string GetType() const { return command.type_; }
+         
+         const std::string GetTaskText() const { return command.task_text_; }
 
-      const int GetId() const { return command.id_;}
+         const int GetId() const { return command.id_;}
 
-      void Parse(const int& argc, const char** argv);
-      
-      //const std::string& GetAllTaskText(int argc, std::string_view argv) const;
-   private:
-      struct Command
-      {
-         //enum Type {ADD, DELETE, LIST, ANOTHER} type_ = ANOTHER;
-         std::string type_ = "";
-         std::string task_text_ = "";
-         int id_ = -1;
-      };
-      Command command;
+         void Parse(const int& argc, const char** argv);
+      private:
+         struct Command
+         {
+            std::string type_ = "";
+            std::string task_text_ = "";
+            int id_ = -1;
+         };
+         Command command;
 };
 
-}  // namespace parse
+}  // namespace parser
